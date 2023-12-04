f134953bf94a45efa125304a79e5b6de93edfd42
Dabidmm <152859554+Dabidmm@users.noreply.github.com>
2023-12-04 13:35:15 -0700

    Actualizar fiel.js

---
+++ fiel.js
@@ -0,0 +1,4 @@
+const user = "$(touser)";
+const per = Math.floor(Math.random() * 100);
+if (user.toLowerCase() === "dabid_mm") 
+{ "@Dabid_MM, eres 0% fiel" } else { `@${user}, eres ${per}% fiel` };
